d18027422935e08f08bd18811046129246120a39
honorezemagho <honorezemagho@gmail.com>
2020-04-04 15:17:59 +0200

    Make a mistake on exercise solved

--- assets/exercise.js
+++ assets/exercise.js
@@ -1,11 +1,11 @@
-// Exercise 1)
+// Exercise 2)
 
 let x = 2;
 for (var y = 0; y < 11; y++) {
     console.log(x + " * " + y + " = " + x * y);
 }
 
-// Exercise 2)
+// Exercise 1)
 
 for (var y = 0; y < 10; y++) {
     console.log("#".repeat(y));
