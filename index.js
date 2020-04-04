ee7ee9df3dc86814a30ed55581e3d07a0a15f904
Prince Eldrige <peldrige8@gmail.com>
2020-04-04 13:37:33 +0100

    Done with the assignements

---
+++ index.js
@@ -0,0 +1,10 @@
+let hash =''
+for (let i = 0; i < 7; i++){
+    hash += '#'
+    console.log(hash)
+}
+
+
+for (let i = 1; i < 10; i ++){
+    console.log(`the value of 2 multiplied by ${i} is ${2 * i}`)
+}
