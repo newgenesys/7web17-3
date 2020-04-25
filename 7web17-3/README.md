9b249f56bfb5848398c02c48d1ee053803ca0bdf
jessik-19 <holtsonj@gmail.com>
2020-04-25 18:26:11 +0200

    mon devoir

---
+++ 7web17-3/README.md
@@ -0,0 +1,4 @@
+# 7web17-3
+Final Exercise for Lesson 17: Program Structure 
+Due Date is Tuesday 7th of April 2020  
+Submission by GitHub
---
+++ calculatrice.js
@@ -0,0 +1,20 @@
+function division (){
+    let a = Number(prompt('enter the first number'))
+    let b = Number(prompt('enter the second number'))
+    alert(`the quotient is: ${a / b}`)
+}
+function multiplication (){
+    let a = Number(prompt('enter the first number'))
+    let b = Number(prompt('enter the second number'))
+    alert(`their produit is: ${a * b}`)
+}
+function soustraction (){
+    let a = Number(prompt('enter the first number'))
+    let b = Number(prompt('enter the second number'))
+    alert(`their difference is: ${a - b}`)
+}
+function addition (){
+    let a = Number(prompt('enter the first number'))
+    let b = Number(prompt('enter the second number'))
+    alert(`their sum is: ${a + b}`)
+}
